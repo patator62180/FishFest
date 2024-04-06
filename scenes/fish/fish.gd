9a5f2f59542fc5eb51b4019c58f286dc70e81259
Martin Briand <martinbriands@gmail.com>
2024-04-06 19:15:06 -0400

    level ++ we getting there

--- scenes/fish/fish.gd
+++ scenes/fish/fish.gd
@@ -4,12 +4,16 @@ class_name Fish
 
 @export var animation_player: AnimationPlayer
 @export var is_moving_transition_speed: float
+@export var is_grounded_transition_speed: float
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var glasses: MeshInstance3D = $Glasses
 
 var is_moving_bool: bool
 var is_moving_float: float
+
+var is_grounded_bool: bool
+var is_grounded_float: float
 
 func _ready():
     glasses.visible = false
@@ -27,7 +31,8 @@ func set_moving(is_moving: bool):
         animation_tree.set("parameters/struggle_height/blend_amount", 1)
 
 func set_grounded(is_grounded: bool):
-    animation_tree.set("parameters/is_grounded/blend_amount", 1 if is_grounded else 0)
+    is_grounded_bool = is_grounded
+    animation_tree.set("parameters/struggle_height/blend_amount", 1)
     
 func put_on_glasses():
     glasses.visible = true
@@ -39,5 +44,12 @@ func _process(delta):
         is_moving_float = is_moving_float - is_moving_transition_speed * delta
     
     is_moving_float = clampf(is_moving_float, 0.0, 1.0)
-    
     animation_tree.set("parameters/is_moving/blend_amount", is_moving_float)
+    
+    if is_grounded_bool and is_grounded_float < 1:
+        is_grounded_float = is_grounded_float + is_grounded_transition_speed * delta
+    elif !is_grounded_bool and is_grounded_float > 0:
+        is_grounded_float = is_grounded_float - is_grounded_transition_speed * delta
+    
+    is_grounded_float = clampf(is_grounded_float, 0.0, 1.0)
+    animation_tree.set("parameters/is_grounded/blend_amount", is_grounded_float)
