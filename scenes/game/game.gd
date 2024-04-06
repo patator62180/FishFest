42035407cf8c0e15adc683a9abae95e6ce513dbc
LeTitouLapinou <162844593+LeTitouLapinou@users.noreply.github.com>
2024-04-06 16:15:04 -0400

    Condition de victoire

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -5,12 +5,20 @@ extends Node3D
 
 func _ready():
     var glasses: Glasses = get_tree().get_first_node_in_group("Glasses")
+    var end_goal: EndGoal = get_tree().get_first_node_in_group("EndGoal")
        
     if glasses != null:
         glasses.glasses_picked_up.connect(on_glasses_picked_up)
-
+        
+    if end_goal != null:
+        end_goal.level_won.connect(on_level_won)
+        
 func on_glasses_picked_up(glasses: Glasses):
     glasses.queue_free()
     if player != null:
         player.put_on_glasses()
     camera_parent.turn_camera()
+
+func on_level_won(end_goal: EndGoal):
+     if(player.glasses_on != 1):
+        print("gagne")
