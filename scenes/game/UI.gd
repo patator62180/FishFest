509786e0cf5f6c36261ec335a3e23c80b781329b
David Corticchiato <david.corticchiato@gmail.com>
2024-04-07 16:10:18 -0400

    add splash music

--- scenes/game/UI.gd
+++ scenes/game/UI.gd
@@ -5,16 +5,22 @@ class_name UI
 @onready var start_UI: Control = $StartUI
 @onready var middle_UI: Control = $MiddleUI
 @onready var end_UI: Control = $EndUI
+@onready var splash_music: AudioStreamPlayer = $SplashMusic
+
+signal start_pressed
 
 func _ready():
     start_UI.visible = true
     middle_UI.visible = false
     end_UI.visible = false
+    splash_music.play()
 
 func _on_start_pressed():
     start_UI.visible = false
-    
     Player.instance.is_dead = false
+    splash_music.stop()
+    start_pressed.emit()
+
 
 func reach_mid_point():
     middle_UI.visible = true
